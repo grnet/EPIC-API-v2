bd8079b1d352fd7cf3779d565e0c2ae6394c974f
Christos Kanellopoulos <skanct@gmail.com>
2013-10-12 09:58:07 +0300

    Added a GRNET specific profile
    A profile that uses an internal (technical) type ('INST') to share a
    prefix between institutes.

--- src/epic_profile.rb
+++ src/epic_profile.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright ©2011-2012 Pieter van Beek <pieterb@sara.nl>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -221,6 +222,94 @@ module EPIC
     
 
     end # class GWDGPID < Profile
+
+    # A profile that uses an internal (technical) type ('INST') to share a prefix between institutes.
+    #
+    # (e.g. GRNET)
+    #
+    class GRNETPID < Profile
+      def to_rackful
+        {
+          'Description' => 'This profile provides support for sharing a prefix between multiple institutes',
+        }
+      end
+
+      def self.create( request, prefix, suffix, values )
+        self.checkUserEntry(request)
+        # We can trust in the existance of a Institute code in the config. Now we create local variables.
+        username = request.env['REMOTE_USER']
+        institute_code = USERS[request.env['REMOTE_USER']][:institute].upcase
+
+        # Enforce the Inst-Recod with an own method
+        values = self.enforce_inst_record(values, institute_code)
+        # self.debug_dump_values(values)
+        values
+      end
+
+      def self.update( request, prefix, suffix, old_values, new_values )
+        self.checkUserEntry(request)
+        username = request.env['REMOTE_USER']
+        institute_code = USERS[request.env['REMOTE_USER']][:institute].upcase
+        # The PID is enforced in the new values
+        new_values = self.enforce_inst_record(new_values, institute_code)
+        # Check if the supplied Inst-Code stored for the user is identical to the Inst-Code of the handle-vaalue that is selected for update
+        inst_check_passed = false
+        old_values.each do |old_val|
+          # Find INST value in old_values
+          if old_val.type.upcase == 'INST'
+            # find INST value in the new_values
+            new_values.each do |new_val|
+              if new_val.type.upcase == 'INST'
+                # The Check is only passed if the INST-fields of the old_val and new_val are identical
+                if new_val.data.upcase == old_val.data.upcase
+                  inst_check_passed = true
+                  break
+                end
+              end
+            end
+          end
+        end
+        # React on failed inst_checks
+        unless inst_check_passed
+          message = "Enforcing GRNET-Profile. Handle update blocked, as the #{username} requested a handle with a missmatching Institute-Code"
+          LOGGER.info(message)
+          raise Rackful::HTTP403Forbidden, message
+        end
+        new_values
+      end
+
+      private
+
+      def self.checkUserEntry(request)
+        # Check if a insitute code is available in the config
+        if USERS[request.env['REMOTE_USER']][:institute].nil?
+          message = "Enforcing GRNET-Profile. No Insitute-Code set for user #{request.env['REMOTE_USER']}. Request blocked with Error 403 - Forbidden."
+          LOGGER.warn(message)
+          raise Rackful::HTTP403Forbidden, message
+        end
+      end
+
+      def self.enforce_inst_record (values, inst_number)
+        # If the submitted a inst_record, then remove it.
+        if values.any? { |v| 'INST' === v.type.upcase }
+          newvalues = []
+          values.each { |value| newvalues << value unless value.type.upcase == 'INST' }
+          values = newvalues
+        end
+        # Now install an own INST-Record given ind inst_number in the handle-value set.
+        idx = 2
+        idx += 1 while values.any? { |v| idx === v.idx }
+        inst_record = HandleValue.new
+        inst_record.idx = idx
+        inst_record.type = 'INST'
+        inst_record.parsed_data = inst_number
+        values << inst_record
+        LOGGER.info("Enforcing GRNET-Profile. Institute-Code #{inst_number} appended to Handle.")
+        values
+      end
+
+
+    end # class GRNETPID < Profile
 
   end # class Profile
 
